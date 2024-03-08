17635ce0a7d78ee461e060c6249b86fd2fa5032e
Muhammed Ali Ud <72108306+Maliud@users.noreply.github.com>
2024-03-08 14:57:03 +0300

    Update openweather_api.dart

--- lib/api/openweather_api.dart
+++ lib/api/openweather_api.dart
@@ -1,7 +1,7 @@
 
 class OpenWeatherAPI {
 
-  String apiKey = 'e37a50392c8b57e68ffb63872d2e64a4';
+  String apiKey = 'Api Key Giriniz// Api Key Here';
 
   String apiUrl(lat, lon) {
     return 'https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&appid=$apiKey';
